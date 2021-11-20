dd6d7d26897678f34ab6aec46c87cea932bb6c8b
Nghi Nguyen <nghintFX12982@funix.edu.vn>
2021-11-20 14:43:53 +0700

    Single Page Application Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,18 +2,24 @@ import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Home from "./HomeComponent";
 import Menu from "./MenuComponent";
+import Contact from "./ContactComponent";
 import DishDetail from "./DishdetailComponent";
 import React, { Component } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { Navbar, NavbarBrand } from "reactstrap";
 import { DISHES } from "../shared/dishes";
+import { COMMENTS } from "../shared/comments";
+import { PROMOTIONS } from "../shared/promotions";
+import { LEADERS } from "../shared/leaders";
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
       dishes: DISHES,
-      selectedDish: null,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
     };
   }
 
@@ -22,16 +28,27 @@ class Main extends Component {
   }
 
   render() {
+    const HomPage = () => {
+      return (
+        <Home
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
+      );
+    };
+
     return (
       <div>
         <Header />
         <Switch>
-          <Route path="/home" component={Home} />
+          <Route path="/home" component={HomPage} />
           <Route
             exat
             path="/menu"
             component={() => <Menu dishes={this.state.dishes} />}
           />
+          <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
